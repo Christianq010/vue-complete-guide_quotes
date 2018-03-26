c020f5f68c0350bb3c494375cfb2e0658d995d0c
Christiaan Quyn <christianq010@gmail.com>
2018-03-26 23:49:09 +0530

    initial commit

---
+++ src/components/Quote.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="col-md-4">
+        <div class="panel panel-default">
+            <div class="panel-body quote">
+                <slot></slot>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['quotes']
+    }
+</script>
+
+<style>
+    .panel-body{
+        font-family: 'Lato', sans-serif;
+        font-size: 24px;
+        color: #6e6e6e;
+    }
+    .quote:hover {
+        background-color: palevioletred;
+    }
+</style>
