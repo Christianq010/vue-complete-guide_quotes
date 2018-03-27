986886c4c4f52556d9d9f21942726ab812c3f09d
Christiaan Quyn <christianq010@gmail.com>
2018-03-27 19:50:08 +0530

    Styling: font, text area, font family, box shadows

--- src/components/NewQuote.vue
+++ src/components/NewQuote.vue
@@ -2,7 +2,6 @@
     <div class="row">
         <form>
             <div class="form-group">
-                <label>Quote</label>
                 <textarea class="form-control" v-model="quote"></textarea>
                 <button class="btn-primary" v-on:click.prevent="createNew">Add Quote</button>
             </div>
@@ -30,13 +29,27 @@
     .row {
         margin: 2em auto;
         text-align: center;
+        font-family: 'Lato', sans-serif;
     }
     textarea {
         margin: 1em auto;
+        font-size: 25px;
+        color: lightgrey;
         width: 60%;
+        box-shadow: 0 16px 28px 0 rgba(0,0,0,.05), 0 25px 55px 0 rgba(0,0,0,.06);
     }
     button {
+        font-size: 25px;
+        font-weight: 900;
         margin: 1em;
+        width: 250px;
+        min-height: 50px;
+        background: #583b8a;
+        position: relative;
+        box-shadow: 0 16px 28px 0 rgba(0,0,0,.05), 0 25px 55px 0 rgba(0,0,0,.06);
+    }
+    button:hover {
+        background: #583b8a;
     }
 
 </style>
