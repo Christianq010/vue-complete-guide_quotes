f250aa0dbe1495db3a3be319f2e5292f73017f3e
Christiaan Quyn <christianq010@gmail.com>
2018-03-29 13:31:55 +0530

    Feat: Progress bar for quote count

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
     <div class="container">
+        <app-header v-bind:quoteCount="quotes.length" v-bind:maxQuotes="maxQuotes"></app-header>
         <app-new-quote v-on:quoteAdded="newQuote"></app-new-quote>
         <app-quote-grid v-bind:quotes="quotes" v-on:quoteDeleted="deleteQuote"></app-quote-grid>
+        <!-- Info Window -->
         <div class="row">
             <div class="col-sm-12 text-center">
                 <div class="alert alert-success alert-edit">
@@ -9,12 +11,14 @@
                 </div>
             </div>
         </div>
+        <!-- /end of Info Window -->
     </div>
 </template>
 
 <script>
     import QuoteGrid from './components/QuoteGrid.vue';
     import NewQuote from './components/NewQuote.vue';
+    import Header from './components/Header.vue';
 
     export default {
         data: function() {
@@ -35,7 +39,8 @@
         },
         components: {
             appQuoteGrid: QuoteGrid,
-            appNewQuote: NewQuote
+            appNewQuote: NewQuote,
+            appHeader: Header
         }
     }
 </script>
---
+++ src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="row">
+        <div class="col-sm-12">
+            <!-- <h3 class="text-center">Quotes Added</h3> -->
+            <div class="progress">
+                <div class="progress">
+                    <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100"
+                        :style="{width: (quoteCount / maxQuotes) * 100 + '%'}">
+                        {{ quoteCount }} / {{ maxQuotes}}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        props: ['quoteCount', 'maxQuotes']
+    }
+</script>
+<style scoped>
+    h3 {
+        color: white;
+        text-shadow: 1px 1px grey;
+    }
+    .row {
+        margin: 1.5em 0 0 0;
+    }
+    .progress-bar {
+        background-color:#6DB193;
+    }
+</style>
